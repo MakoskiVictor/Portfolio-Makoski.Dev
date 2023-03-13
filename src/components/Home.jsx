2264a5df4bcfc0243f1d2868199dbc003803008e
MakoskiVictor <makoski.ed@gmail.com>
2023-03-13 12:36:26 -0300

    added classnames

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,25 +3,27 @@ import HomeStyles from '@/styles/Home.module.css'
 
 export function Home () {
   return (
-    <section className={HomeStyles.home}>
-      <div className={HomeStyles.texts}>
-        <div className={HomeStyles.saluteContainer}>
-          <p className={HomeStyles.salute}>Hi, I'm Víctor 🙋‍♂️ </p>
+    <section className={HomeStyles.section}>
+      <div className={HomeStyles.home}>
+        <div className={HomeStyles.texts}>
+          <div className={HomeStyles.saluteContainer}>
+            <p className={HomeStyles.salute}>Hi, I'm Víctor 🙋‍♂️ </p>
+          </div>
+          <div>
+            <p className={HomeStyles.fullStack}>Full Stack Developer</p>
+          </div>
+          <div>
+            <p className={HomeStyles.paragraph}>I love being able to learn something everyday by engaging in experiments and working on projects.
+              It's not only a job, is a passion.
+            </p>
+          </div>
+          <div className={HomeStyles.buttonContainer}>
+            <button className={HomeStyles.button}>Work with me</button>
+          </div>
         </div>
-        <div>
-          <p className={HomeStyles.fullStack}>Full Stack Developer</p>
+        <div className={HomeStyles.image}>
+          <img src={webDesigner} alt='web designer img' className={HomeStyles.imageHome} />
         </div>
-        <div>
-          <p className={HomeStyles.paragraph}>I love being able to learn something everyday by engaging in experiments and working on projects.
-            It's not only a job, is a passion.
-          </p>
-        </div>
-        <div className={HomeStyles.buttonContainer}>
-          <button className={HomeStyles.button}>Work with me</button>
-        </div>
-      </div>
-      <div className={HomeStyles.image}>
-        <img src={webDesigner} alt='web designer img' className={HomeStyles.imageHome} />
       </div>
     </section>
   )
