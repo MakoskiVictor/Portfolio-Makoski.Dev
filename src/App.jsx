44276db42e1751ea1615c5dd1dcada1ad6918334
MakoskiVictor <makoski.ed@gmail.com>
2023-03-15 18:19:29 -0300

    skills section created

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import { NavBar } from '@/components/NavBar.jsx'
 import { Home } from '@/components/Home.jsx'
 import { About } from '@/components/About.jsx'
+import { SKILLS } from '@/utils.js'
+import skillsStyle from '@/styles/Skills.module.css'
 
 export function App () {
   return (
@@ -9,6 +11,21 @@ export function App () {
       <NavBar />
       <Home />
       <About />
+      <section className={skillsStyle.section}>
+        <div className={skillsStyle.skills}>
+          {
+          SKILLS.map(skill => {
+            return (
+              <a href={skill.web} key={skill.name}>
+                <div className={skillsStyle.imageContainer}>
+                  <img src={skill.image} alt={`${skill.name} icon`} className={skillsStyle.image} />
+                </div>
+              </a>
+            )
+          })
+        }
+        </div>
+      </section>
     </main>
   )
 }
