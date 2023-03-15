8856f71f8854666413359d993ef8e4ba5f105755
MakoskiVictor <makoski.ed@gmail.com>
2023-03-15 17:28:22 -0300

    utils with skills

---
+++ src/utils.js
@@ -0,0 +1,43 @@
+import javascript from '@/assets/skills/javascript.svg'
+import css from '@/assets/skills/css.svg'
+import html from '@/assets/skills/html.svg'
+import react from '@/assets/skills/react.svg'
+import redux from '@/assets/skills/redux.svg'
+import git from '@/assets/skills/git.svg'
+import node from '@/assets/skills/node.svg'
+import postgres from '@/assets/skills/postgres.svg'
+
+export const SKILLS = [
+  {
+    name: 'Javascript',
+    image: javascript
+  },
+  {
+    name: 'CSS',
+    image: css
+  },
+  {
+    name: 'HTML',
+    image: html
+  },
+  {
+    name: 'react',
+    image: react
+  },
+  {
+    name: 'redux',
+    image: redux
+  },
+  {
+    name: 'git',
+    image: git
+  },
+  {
+    name: 'node',
+    image: node
+  },
+  {
+    name: 'postgres',
+    image: postgres
+  }
+]
