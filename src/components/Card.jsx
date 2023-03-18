a093035c55201f23c45fda08b97be36be7f01712
MakoskiVictor <makoski.ed@gmail.com>
2023-03-18 16:10:01 -0300

    created card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,25 @@
+import portfolioStyles from '@/styles/Portfolio.module.css'
+import { PROJECTS } from '@/utils.js'
+
+export function Card () {
+  return (
+    <div className={portfolioStyles.bottom}>
+      {
+              PROJECTS.map(project => {
+                return (
+                  <a href={project.url} key={project.name} className={portfolioStyles.cardContainer}>
+                    <div className={portfolioStyles.imageContainer}>
+                      <img src={project.image} alt={project.name} className={portfolioStyles.projectImage} />
+                    </div>
+                    <div>
+                      <p className={portfolioStyles.projectName}>{project.name}</p>
+                      <p className={portfolioStyles.projectDescription}>{project.description}</p>
+                    </div>
+                    <div className={portfolioStyles.linea} />
+                  </a>
+                )
+              })
+            }
+    </div>
+  )
+}
