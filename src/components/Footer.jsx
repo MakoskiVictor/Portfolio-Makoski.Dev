7746b209d344e80ac2e384b4d10a3e9c1bfc33ba
MakoskiVictor <makoski.ed@gmail.com>
2023-03-19 14:56:43 -0300

    Created footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import footerStyles from '@/styles/Footer.module.css'
+import GithubLogo from '@/assets/Logos/GithubLogo.svg'
+import LinkedinLogo from '@/assets/Logos/LinkedinLogo.svg'
+
+export function Footer () {
+  return (
+    <section className={footerStyles.section}>
+      <div className={footerStyles.container}>
+        <p className={footerStyles.name}>Makoski Víctor</p>
+        <div>
+          <ul className={footerStyles.ul}>
+            <li className={footerStyles.socialsLogo}>
+              <a href='https://github.com/MakoskiVictor'><img src={GithubLogo} alt='GitHub Logo' className={footerStyles.img} /></a>
+            </li>
+            <li className={footerStyles.socialsLogo}><a href='https://www.linkedin.com/in/makoskidev/'><img src={LinkedinLogo} alt='LinkedIn Logo' className={footerStyles.img} /></a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  )
+}
