b3cd85aee64b8a8f8bad3642ba354bb2e0e52f10
MakoskiVictor <makoski.ed@gmail.com>
2023-03-21 09:57:59 -0300

    messeger componend created

--- src/components/Messager.jsx
+++ src/components/Messager.jsx
@@ -1,7 +1,56 @@
+import { useState } from 'react'
+import messagerStyles from '@/styles/Messager.module.css'
+import emailjs from '@emailjs/browser'
+
 export function Messager () {
-  return (
-    <section>
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [subject, setSubject] = useState('')
+  const [message, setMessage] = useState('')
+
+  const handleChange = (e) => {
+    if (e.target.name === 'name') setName(e.target.value)
+    if (e.target.name === 'email') setEmail(e.target.value)
+    if (e.target.name === 'subject') setSubject(e.target.value)
+    if (e.target.name === 'message') setMessage(e.target.value)
+    console.log(name, email, subject, message)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    emailjs.send('service_p29qgw1', 'template_wblizpm', {
+      from_name: name,
+      from_subject: subject,
+      from_mail: email,
       message
-    </section>
+    }, 'x9ZTYKf99VqNZsdGN').then(res => {
+      console.log(res)
+    })
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className={messagerStyles.form}>
+      <div>
+        <div className={messagerStyles.container}>
+          <div>
+            <input type='text' placeholder='Your name' name='name' onChange={(e) => handleChange(e)} value={name} className={messagerStyles.name} />
+          </div>
+          <div>
+            <input type='text' placeholder='Your email' name='email' onChange={(e) => handleChange(e)} className={messagerStyles.mail} />
+          </div>
+        </div>
+        <div>
+          <div className={messagerStyles.container}>
+            <input type='text' placeholder='Subject' name='subject' onChange={(e) => handleChange(e)} className={messagerStyles.subject} />
+          </div>
+          <div className={messagerStyles.container}>
+            <textarea cols='30' rows='10' placeholder='Your message' name='message' onChange={(e) => handleChange(e)} className={messagerStyles.message} />
+          </div>
+        </div>
+      </div>
+      <div className={messagerStyles.buttonContainer}>
+        <button type='submit' className={messagerStyles.button}>Send</button>
+      </div>
+    </form>
   )
 }
